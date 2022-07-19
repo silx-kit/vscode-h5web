d9716bdb3d68631f7879f9ad0d52f0a1f8058ad0
Axel Bocciarelli <axelboc@gmail.com>
2022-07-19 12:03:28 +0200

    Allow opening files outside of workspace folder (#3)
    * Allow opening files outside of workspace folder
    
    * Update src/H5WebViewer.ts
    
    Co-authored-by: Loïc Huder <42204205+loichuder@users.noreply.github.com>
    
    Co-authored-by: Loïc Huder <42204205+loichuder@users.noreply.github.com>

--- src/H5WebViewer.ts
+++ src/H5WebViewer.ts
@@ -32,7 +32,19 @@ export default class H5WebViewer
     webviewPanel: WebviewPanel
   ): Promise<void> {
     const { webview } = webviewPanel;
-    webview.options = { enableScripts: true };
+
+    // Allow opening files outside of workspace
+    // https://github.com/ucodkr/vscode-tiff/blob/9a4f976584fcba24e9f25680fcdb47fc8f97493f/src/tiffPreview.ts#L27-L30
+    const extensionRoot = Uri.file(this.context.extensionPath);
+    const resourceRoot = document.uri.with({
+      path: document.uri.path.replace(/\/[^/]+?\.\w+$/, '/'),
+    });
+
+    webview.options = {
+      enableScripts: true,
+      localResourceRoots: [resourceRoot, extensionRoot],
+    };
+
     webview.html = await this.getHtmlForWebview(webview);
 
     webview.onDidReceiveMessage((evt) => {
