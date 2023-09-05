8f2ca79823ec5a360c7f0a81d27d5d2b2691614f
Axel Bocciarelli <axelboc@gmail.com>
2023-09-05 16:19:33 +0200

    Support exporting to JSON

--- app/utils.ts
+++ app/utils.ts
@@ -1,10 +1,24 @@
-import { FileInfo, Message } from './models';
+import type { GetExportURL } from '@h5web/app';
+import { MessageType } from '../src/models.js';
+import { vscode } from './vscode-api.js';
 
 // 2 GB = 2 * 1024 * 1024 * 1024 B
 export const MAX_SIZE_IN_BYTES = 2147483648;
 
-export function isFileInfoMessage(
-  message: Message
-): message is Message<FileInfo> {
-  return message.type === 'FileInfo';
-}
+export const getExportURL: GetExportURL = (format, dataset, __, value) => {
+  if (format === 'json') {
+    return async () => {
+      vscode.postMessage({
+        type: MessageType.Export,
+        data: {
+          format,
+          name: dataset.name,
+          payload: JSON.stringify(value, null, 2),
+        },
+      });
+      return new Blob(); // doesn't matter as long as it's not falsy
+    };
+  }
+
+  return undefined;
+};
---
+++ src/models.ts
@@ -0,0 +1,24 @@
+import { ExportFormat } from '@h5web/app';
+
+export type Message =
+  | { type: MessageType.Ready }
+  | { type: MessageType.FileInfo; data: FileInfo }
+  | { type: MessageType.Export; data: Export };
+
+export enum MessageType {
+  Ready = 'Ready',
+  FileInfo = 'FileInfo',
+  Export = 'Export',
+}
+
+export interface FileInfo {
+  uri: string;
+  name: string;
+  size: number;
+}
+
+export interface Export {
+  format: ExportFormat;
+  name: string;
+  payload: string;
+}
