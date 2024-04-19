650cad6ad337c347bc1522b640c0f91b260bd89d
Axel Bocciarelli <axelboc@gmail.com>
2024-04-19 11:44:46 +0200

    Upgrade H5Web and fix error display when file size limit is reached

--- app/Viewer.tsx
+++ app/Viewer.tsx
@@ -4,12 +4,26 @@ import { suspend } from 'suspend-react';
 import { getExportURL, getPlugin } from './utils';
 import { type FileInfo } from '../src/models.js';
 
+// 2 GB = 2 * 1024 * 1024 * 1024 B
+const MAX_SIZE_IN_BYTES = 2147483648;
+
 interface Props {
   fileInfo: FileInfo;
 }
 
 function Viewer(props: Props) {
   const { fileInfo } = props;
+
+  if (fileInfo.size === 0) {
+    // e.g. when comparing git changes on an untracked file - https://github.com/silx-kit/vscode-h5web/issues/22
+    return <p>File does not exist</p>;
+  }
+
+  if (fileInfo.size >= MAX_SIZE_IN_BYTES) {
+    throw new Error(
+      'Cannot open: the file is bigger than the maximum supported size (2 GB)'
+    );
+  }
 
   const buffer = suspend(async () => {
     const res = await fetch(fileInfo.uri);
--- app/utils.ts
+++ app/utils.ts
@@ -8,9 +8,6 @@ assertNonNull(pluginsScriptElem);
 
 const PLUGINS = JSON.parse(pluginsScriptElem.innerHTML);
 
-// 2 GB = 2 * 1024 * 1024 * 1024 B
-export const MAX_SIZE_IN_BYTES = 2147483648;
-
 export async function getPlugin(
   name: Plugin
 ): Promise<ArrayBuffer | undefined> {
