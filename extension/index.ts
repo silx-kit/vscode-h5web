7b304439334e05187d0bed7cd647057cf4379067
Axel Bocciarelli <axelboc@gmail.com>
2025-03-31 11:47:25 +0200

    Log warning when output file is too big to open

--- extension/index.ts
+++ extension/index.ts
@@ -5,12 +5,18 @@ import H5WebViewer from './H5WebViewer';
 const EDITOR_IDS = ['h5web.viewer', 'h5web.fallback-viewer'];
 
 export function activate(context: ExtensionContext): void {
+  const outputChannel = window.createOutputChannel('H5Web', { log: true });
+
   context.subscriptions.push(
     ...EDITOR_IDS.map((id) =>
-      window.registerCustomEditorProvider(id, new H5WebViewer(context), {
-        webviewOptions: { retainContextWhenHidden: true },
-        supportsMultipleEditorsPerDocument: true,
-      }),
+      window.registerCustomEditorProvider(
+        id,
+        new H5WebViewer(context, outputChannel),
+        {
+          webviewOptions: { retainContextWhenHidden: true },
+          supportsMultipleEditorsPerDocument: true,
+        },
+      ),
     ),
   );
 }
